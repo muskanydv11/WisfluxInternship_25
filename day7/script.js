b79968e53dd90e16ab970f019936798a68e189c8
muskanydv11 <yadav.muskan1978@gmail.com>
2025-06-18 17:54:59 +0530

    Create script.js

---
+++ day7/script.js
@@ -0,0 +1,49 @@
+console.log("Javascript Variables")
+var a=5;
+var b=6;
+var c="Muskan";
+// var 55a="MUSKAN"   NOT ALLOWED
+console.log(typeof a ,typeof b,typeof c)
+console.log(a + b + 11 + c)
+
+//const a1=6;
+//a1=a1+1//not allowed
+
+
+let a1=5;
+let b1=6;
+let c1="Muskan";
+console.log(a1+b1+c1)
+console.log(typeof a1,typeof b1,typeof c1)
+
+{
+
+    let a=10
+    let c1=20
+    console.log(a)
+    console.log(c1)
+}
+console.log(a,b1)
+
+
+let x="Muskan Yadav";
+let y = 20;
+let z = 6.5;
+const p=true;
+let q =undefined;
+let r=null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z , typeof p,typeof q,typeof r)
+
+let o = {
+    name:"muskan",
+    "job code": 5000
+}
+console.log(o);
+o.salary="1cr";
+console.log(o);
+o.salary="5cr";
+console.log(o);
+
+
+
