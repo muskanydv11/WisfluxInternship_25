b49bf8d10c43d25f88fdbe7a9b1665039ccf7919
muskanydv11 <yadav.muskan1978@gmail.com>
2025-06-21 17:55:21 +0530

    Create Navbar.jsx

---
+++ React/components,props,JSX/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Navbar.css"
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CONTACT US</li>
+          </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
