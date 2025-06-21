6b1ea47436e80f9ac6b73b96bf472377332bf0b6
muskanydv11 <yadav.muskan1978@gmail.com>
2025-06-21 17:57:51 +0530

    Create Footer.jsx

---
+++ React/components,props,JSX/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./footer.css"
+
+const footer = () => {
+  return (
+    <>
+    <div className='Footer'>
+      copyright &copy; www.muskanyadav.com | All rights reserved
+    </div>
+    </>
+  )
+}
+
+export default footer
